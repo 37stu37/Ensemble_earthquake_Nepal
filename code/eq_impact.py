8ebedc0b76685aec32e91dbae7ad822a415ad621
37stu37 <37stu37@gmail.com>
2023-03-02 14:50:59 +0000

    added landslide scenario to ray multiprocessing

--- code/eq_impact.py
+++ code/eq_impact.py
@@ -1,3 +1,7 @@
+from numba import njit
+import numpy as np
+
+
 def eq_impact(bldgs,
               bldgs_vuln,
               shake_ras,
@@ -24,3 +28,66 @@ def eq_impact(bldgs,
     b['low'], b['mid'], b['high'] = weighted_probability_of_collapse(t, n, b[shake_ras].values)
 
     return b
+
+
+@njit()
+def mh_cascade_scenario(EXP_IDS, PPGA_SU, SI_SU, FLOWR_MEAN, FLOWR_STD):
+    a = np.zeros(EXP_IDS.shape[0], np.float64)
+
+    for i, _ in enumerate(EXP_IDS):
+        rng = np.random.uniform(0, 1)
+        if PPGA_SU[i] > rng:
+            rng = np.random.uniform(0, 1)
+            if np.random.normal(SI_SU[i], SI_SU[i] * 0.1) > rng:
+                rng = np.random.uniform(0, 1)
+                if np.random.normal(FLOWR_MEAN[i], FLOWR_STD[i]) > rng:
+                    a[i] = 1
+    return a
+
+
+def ls_impact(bldgs,
+              slopes,
+              shake_ras,
+              shake_dir,
+              flowr_stats,
+              n_scenarios):
+    from scipy.stats import lognorm
+    import pandas as pd
+    from mods_geom_ops import Pcentroid_Rsampling, join_Pcentroid_at_Pl
+
+    # sample PGA values at slope unit
+    slopes_pga = Pcentroid_Rsampling(slopes, shake_ras, shake_dir)
+
+    # probability of slope triggering landslide from specific PGA
+    # parameters from best fit with true positive from Gorkha
+    # from /Users/alexdunant/Documents/Github/SN_MH-methodology/QC.ipynb
+    μ = 0.5129102564102563
+    σ = 0.12724392947057922
+    slopes_pga["pPGA_su"] = lognorm(μ, scale=σ).cdf(slopes_pga[f'{shake_ras}'])
+
+    # slope attributes to building dataset
+    bldgs = join_Pcentroid_at_Pl(bldgs,
+                                 ["osm_id", "DISTRICT", "geometry"],
+                                 slopes_pga,
+                                 ["su_id", "SI", "pPGA_su", "geometry"])
+
+    # add FlowR values to osm
+    bldgs = pd.merge(bldgs, flowr_stats, on="osm_id", how="left")
+
+    # generate landslide scenarios for the earthquake
+    ls_impacts = []
+    scenarios_results = np.zeros(bldgs.osm_id.values.shape[0], np.float64)
+
+    # Monte Carlo simulation of landslides for earthquake
+    for n in range(n_scenarios):
+        scenario_impact = mh_cascade_scenario(EXP_IDS=bldgs.osm_id.values,
+                                              PPGA_SU=bldgs.pPGA_su.values,
+                                              SI_SU=bldgs.SI.values,
+                                              FLOWR_MEAN=bldgs.FlowR_mean.values,
+                                              FLOWR_STD=bldgs.FlowR_std.values)
+
+        scenarios_results = scenarios_results + scenario_impact
+
+    bldgs['impact'] = scenarios_results / n_scenarios
+
+    return bldgs
--- code/mcpu_ensemble_impacts_ray.py
+++ code/mcpu_ensemble_impacts_ray.py
@@ -2,6 +2,10 @@ import ray
 from pathlib import Path
 import os
 import geopandas as gpd
+import pandas as pd
+import numpy as np
+from numba import njit
+
 
 # Define the function to run in parallel
 @ray.remote
@@ -28,9 +32,75 @@ def eq_impact(bldgs,
     n, t = append_vulnerability(b, bldgs_vuln)
 
     # extract the probabilities of collapse and attached to buildings
-    b[f'low_{shake_ras}'], b[f'mid_{shake_ras}'], b[f'high_{shake_ras}'] = weighted_probability_of_collapse(t, n, b[shake_ras].values)
+    b[f'low_{shake_ras}'], b[f'mid_{shake_ras}'], b[f'high_{shake_ras}'] = weighted_probability_of_collapse(t, n, b[
+        shake_ras].values)
 
     return b
+
+
+@njit()
+def mh_cascade_scenario(EXP_IDS, PPGA_SU, SI_SU, FLOWR_MEAN, FLOWR_STD):
+    a = np.zeros(EXP_IDS.shape[0], np.float64)
+
+    for i, _ in enumerate(EXP_IDS):
+        rng = np.random.uniform(0, 1)
+        if PPGA_SU[i] > rng:
+            rng = np.random.uniform(0, 1)
+            if np.random.normal(SI_SU[i], SI_SU[i] * 0.1) > rng:
+                rng = np.random.uniform(0, 1)
+                if np.random.normal(FLOWR_MEAN[i], FLOWR_STD[i]) > rng:
+                    a[i] = 1
+    return a
+
+
+@ray.remote
+def ls_impact(bldgs,
+              slopes,
+              shake_ras,
+              shake_dir,
+              flowr_stats,
+              n_scenarios):
+
+    from scipy.stats import lognorm
+    import pandas as pd
+    from mods_geom_ops import Pcentroid_Rsampling, join_Pcentroid_at_Pl
+    from tqdm import trange
+
+    # sample PGA values at slope unit
+    slopes_pga = Pcentroid_Rsampling(slopes, shake_ras, shake_dir)
+
+    # probability of slope triggering landslide from specific PGA
+    # parameters from best fit with true positive from Gorkha
+    # from /Users/alexdunant/Documents/Github/SN_MH-methodology/QC.ipynb
+    μ = 0.5129102564102563
+    σ = 0.12724392947057922
+    slopes_pga["pPGA_su"] = lognorm(μ, scale=σ).cdf(slopes_pga[f'{shake_ras}'])
+
+    # slope attributes to building dataset
+    bldgs = join_Pcentroid_at_Pl(bldgs,
+                                 ["osm_id", "DISTRICT", "geometry"],
+                                 slopes_pga,
+                                 ["su_id", "SI", "pPGA_su", "geometry"])
+
+    # add FlowR values to osm
+    bldgs = pd.merge(bldgs, flowr_stats, on="osm_id", how="left")
+
+    # generate landslide scenarios for the earthquake
+    scenarios_results = np.zeros(bldgs.osm_id.values.shape[0], np.float64)
+
+    # Monte Carlo simulation of landslides for earthquake
+    for n in trange(n_scenarios):
+        scenario_impact = mh_cascade_scenario(EXP_IDS=bldgs.osm_id.values,
+                                              PPGA_SU=bldgs.pPGA_su.values,
+                                              SI_SU=bldgs.SI.values,
+                                              FLOWR_MEAN=bldgs.FlowR_mean.values,
+                                              FLOWR_STD=bldgs.FlowR_std.values)
+
+        scenarios_results = scenarios_results + scenario_impact
+
+    bldgs['impact'] = scenarios_results / n_scenarios
+
+    return bldgs
 
 
 ###################################################################
@@ -43,6 +113,10 @@ rasdir = GDrive / 'tif'
 bldg = gpd.read_file(datadir / "bldgs_preprocs_light_districts [NatBoundary].shp")
 vuln = gpd.read_file(datadir / "npl-ic-exp-dist_UTM45.shp")
 vuln = vuln[["size_dist", "cnt_dist", "geometry"]]
+slope = gpd.read_file(datadir / "slopeunits_preprocs [Natboundary].shp")
+slope = slope[["su_id", "SI", "geometry"]]
+fR_stats = pd.read_csv(datadir / "bldgs_fR_stats.csv")
+fR_stats = fR_stats[["osm_id", "FlowR_mean", "FlowR_std"]]
 list_rasters = [file for file in os.listdir(rasdir) if file.lower().endswith(".tif")]
 
 ###################################################################
@@ -52,23 +126,33 @@ list_rasters = [file for file in os.listdir(rasdir) if file.lower().endswith(".t
 ray.init()
 
 # Create a list to hold the output futures
-output_futures = []
+output_eq_futures = []
+output_ls_futures = []
 
 # Iterate through the inputs and run the function in parallel
-for raster in list_rasters[:10]:
+for raster in list_rasters:
     # Call the function asynchronously and append the output future to the list
-    output_futures.append(eq_impact.remote(bldg.head(1000),
-                                           vuln,
-                                           raster,
-                                           rasdir))
+    output_eq_futures.append(eq_impact.remote(bldg,
+                                              vuln,
+                                              raster,
+                                              rasdir))
+
+    output_ls_futures.append(ls_impact.remote(bldg,
+                                              slope,
+                                              raster,
+                                              rasdir,
+                                              fR_stats,
+                                              10000))
 
 # Wait for all the output futures to complete
-outputs = ray.get(output_futures)
+outputs_eq = ray.get(output_eq_futures)
+outputs_ls = ray.get(output_ls_futures)
 
 # write building datasets to disk
-for i, output in enumerate(outputs):
+for index, (output_eq, output_ls) in enumerate(zip(outputs_eq, outputs_ls)):
     # Write the DataFrame to a CSV file
-    output.to_csv(GDrive / 'results' / f'output_{list_rasters[i]}.csv', index=False)
+    output_eq.to_csv(GDrive / 'results' / f'{list_rasters[index][8:-4]}__eqImpact.csv', index=False)
+    output_ls.to_csv(GDrive / 'results' / f'{list_rasters[index][8:-4]}__lsImpact.csv', index=False)
 
-    # Shutdown Ray
-    ray.shutdown()
+# Shutdown Ray
+ray.shutdown()
